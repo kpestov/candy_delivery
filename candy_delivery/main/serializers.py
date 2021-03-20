60a654e25b8d7ee43c5a075e44fc4e55017cca61
Kirill Pestov <Pestov.K.E@sberbank.ru>
2021-03-20 20:00:18 +0500

    Add ability for bulk creation of m2m couriers and regions

--- candy_delivery/main/serializers.py
+++ candy_delivery/main/serializers.py
@@ -43,9 +43,9 @@ class CourierListSerializer(base_serializers.Serializer):
     def _bulk_create_couriers(self, validated_couriers):
         return Courier.objects.bulk_create([
             Courier(
-                id=validated_courier_data['id'],
-                courier_type=validated_courier_data['courier_type'],
-                working_hours=validated_courier_data['working_hours'],
+                id=validated_courier_data.get('id'),
+                courier_type=validated_courier_data.get('courier_type'),
+                working_hours=validated_courier_data.get('working_hours'),
 
             ) for validated_courier_data in validated_couriers
         ])
@@ -59,9 +59,22 @@ class CourierListSerializer(base_serializers.Serializer):
             Region(id=region_id) for region_id in set(chain.from_iterable(region_ids))
         ])
 
+    def _bulk_create_couriers_regions(self, validated_couriers):
+        region_to_courier_links = []
+        for validated_courier_data in validated_couriers:
+            courier_id, courier_regions = validated_courier_data.get('id'), validated_courier_data.get('regions')
+            for region_id in courier_regions:
+                new_region_courier = Region.couriers.through(
+                    courier_id=courier_id,
+                    region_id=region_id,
+                )
+                region_to_courier_links.append(new_region_courier)
+        Region.couriers.through.objects.bulk_create(region_to_courier_links)
+
     def create(self, data):
         validated_couriers = data.get('data')
         with transaction.atomic():
             created_couriers = self._bulk_create_couriers(validated_couriers)
             self._bulk_create_regions(validated_couriers)
+            self._bulk_create_couriers_regions(validated_couriers)
         return created_couriers
